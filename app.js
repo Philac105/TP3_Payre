2174ac9dc9c82393a6db675928e1e79818bfb5e2
Philippe Lacasse <philac105@hotmail.com>
2022-09-01 17:08:38 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// imports
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT // 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+app.use(express.static('uploads'));
+
+// database connection
+mongoose.connect(process.env.DB_URI)
+.then(() => console.log('Connected to the database'))
+.catch(error => console.log(error));
+
+// routes prefix
+app.use('/api/post', require('./routes/routes'));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname+'/dist'));
+    app.get('*', (req, res) => {
+        res.sendFile(__dirname+"/dist/index.html");
+    })
+}
+
+//start server
+app.listen(port, () => console.log('server running'));
